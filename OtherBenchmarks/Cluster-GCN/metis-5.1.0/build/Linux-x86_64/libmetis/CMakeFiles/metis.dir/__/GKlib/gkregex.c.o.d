4c51b8066b3b097f07a4455bd46a451e3fae5eae
christopheradnel414 <christopheradnel414@gmail.com>
2023-08-13 23:59:40 +0100

    cleaning up

--- OtherBenchmarks/Cluster-GCN/metis-5.1.0/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o.d
+++
@@ -1,3 +0,0 @@
-libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o: \
- /home/chris/Desktop/Cluster-GCN-Code/metis-5.1.0/GKlib/gkregex.c \
- /usr/include/stdc-predef.h
--- OtherBenchmarks/Cluster-GCN/metis-5.1.0/metis-test.py
+++
@@ -1,4 +0,0 @@
-import networkx as nx
-import metis
-G = metis.example_networkx()
-(edgecuts, parts) = metis.part_graph(G, 3)
